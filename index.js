4410029514441003687a903a5f326857b4d1eff8
Aleksi134 <aleleskinen11@gmail.com>
2020-08-18 16:32:23 +0300

    reformat HTML,CSS changes, JS first lines of code

---
+++ index.js
@@ -0,0 +1,12 @@
+var enterbutton = document.getElementById("enter");
+var input = document.getElementById("userInputField");
+var ul = document.querySelector("ul");
+var li = document.getElementsByTagName("li");
+
+function inputLength() {
+    return input.value.length;
+}
+
+function listLength(){
+	return item.length;
+}
