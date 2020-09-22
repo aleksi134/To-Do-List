337f54551e9422c21a257f5d8209ecc6dcdf2ec7
Aleksi134 <aleleskinen11@gmail.com>
2020-09-22 09:05:44 +0300

    some changes

--- index.js
+++ index.js
@@ -1,12 +1,2 @@
-var enterbutton = document.getElementById("enter");
-var input = document.getElementById("userInputField");
-var ul = document.querySelector("ul");
-var li = document.getElementsByTagName("li");
 
-function inputLength() {
-    return input.value.length;
-}
 
-function listLength(){
-	return item.length;
-}
